bc2f3ec8fe3ecd350b55a34e7a00f4887a214c06
noga <nogaschw@post.bgu.ac.il>
2023-04-02 21:55:04 +0300

    create user facade

--- src/Version 1/SadnaExpress/DomainLayer/User/IUserFacade.cs
+++ src/Version 1/SadnaExpress/DomainLayer/User/IUserFacade.cs
@@ -1,8 +1,27 @@
-
 namespace SadnaExpress.DomainLayer.User
 {
     public interface IUserFacade
     {
-
+        void enter();
+        void exit();
+        void register(string email, string firstName, string lastLame, string password);
+        void login(string email, string password);
+        void logout();
+        void addItemToBag(string storeName, string itemName);
+        Dictionary<string,List<string>> getDetailsOnCart();
+        void purchaseCart();
+        void addItemCart(string storeName, string itemName);
+        void removeCart(string storeName, string itemName);
+        void editItemCart(string storeName, string itemName);
+        void openStore(string storeName);
+        void addReview(string storeName, string itemName);
+        void addItemInventory(string storeName, string itemName);
+        void removeItemInventory(string storeName, string itemName);
+        void editItemInventory(string storeName, string itemName);
+        void addOwner(string storeName, string email);
+        void addManager(string storeName, string email);
+        void addPermissionsToManager(string storeName, string email, string Permission);
+        void closeStore(string storeName);
+        void getDetailsOnStore(string storeName);
     }
 }
