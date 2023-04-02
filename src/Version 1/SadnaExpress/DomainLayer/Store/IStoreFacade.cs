9bf507ee64ce3a06e7cc344e1bb3a4bd01e13112
noga <nogaschw@post.bgu.ac.il>
2023-04-02 21:32:57 +0300

    change from product to item

--- src/Version 1/SadnaExpress/DomainLayer/Store/IStoreFacade.cs
+++ src/Version 1/SadnaExpress/DomainLayer/Store/IStoreFacade.cs
@@ -4,20 +4,20 @@ namespace ConsoleApp1.DomainLayer;
 
 public interface IStoreFacade
 {
-    public void openNewShop(string storeName);
+    public void openNewStore(string storeName);
     public void closeStore(string storeName);
     public void purchaseItems(string storeName, List<string> itemsName);
     public void getStoreHistory(string storeName);
-    public void addProduct(string storeName, string itemName, string category, int price);
-    public void removeProduct(string storeName, string itemName, string category, int price);
-    public void editProductCategory(string storeName, string itemName, string category);
-    public void editProductPrice(string storeName, string itemName, int price);
-    public List<Item> getProductsByName(string itemName);
-    public List<Item> getProductsByCategory(string category);
-    public List<Item> getProductsByKeysWord(string keyWords);
-    public List<Item> getProductsByPrices(int minPrice, int maxPrice);
-    public List<Item> getProductsByItemRating(int rating);
-    public List<Item> getProductsByStoreRating(int rating);
-    public void reviewProduct(string storeName, string itemName, int rating);
+    public void addItem(string storeName, string itemName, string category, int price);
+    public void removeItem(string storeName, string itemName, string category, int price);
+    public void editItemCategory(string storeName, string itemName, string category);
+    public void editItemPrice(string storeName, string itemName, int price);
+    public List<Item> getItemsByName(string itemName);
+    public List<Item> getItemsByCategory(string category);
+    public List<Item> getItemsByKeysWord(string keyWords);
+    public List<Item> getItemsByPrices(int minPrice, int maxPrice);
+    public List<Item> getItemsByItemRating(int rating);
+    public List<Item> getItemsByStoreRating(int rating);
+    public void reviewItem(string storeName, string itemName, int rating);
 
 }
