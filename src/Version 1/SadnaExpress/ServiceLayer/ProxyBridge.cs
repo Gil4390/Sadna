ded7a00ec6cd26b2c605dd0256ae1d1e7dcb6fb7
shaykres <shayk1934@gmail.com>
2023-04-06 23:30:33 +0300

    add all functions to ITradingSystem

--- src/Version 1/SadnaExpress/ServiceLayer/ProxyBridge.cs
+++ src/Version 1/SadnaExpress/ServiceLayer/ProxyBridge.cs
@@ -1,4 +1,5 @@
-﻿using SadnaExpress.Services;
+﻿using SadnaExpress.ServiceLayer.ServiceObjects;
+using SadnaExpress.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -79,6 +80,111 @@ namespace SadnaExpress.ServiceLayer
         public ResponseT<int> Logout(int id)
         {
             return _realBridge.Logout(id);
+        }
+
+        public ResponseT<List<S_Store>> GetAllStoreInfo(int id)
+        {
+            return _realBridge.GetAllStoreInfo(id);
+        }
+
+        public Response AddItemToCart(int id, int itemID, int itemAmount)
+        {
+            return _realBridge.AddItemToCart(id, itemID, itemAmount);
+        }
+
+        public Response PurchaseCart(int id, string paymentDetails)
+        {
+            return _realBridge.PurchaseCart(id, paymentDetails);
+        }
+
+        public Response CreateStore(int id, string storeName)
+        {
+            return _realBridge.CreateStore(id, storeName);
+        }
+
+        public Response WriteReview(int id, int itemID, string review)
+        {
+            return _realBridge.WriteReview(id, itemID, review);
+        }
+
+        public Response RateItem(int id, int itemID, int score)
+        {
+            return _realBridge.RateItem(id, itemID, score);
+        }
+
+        public Response WriteMessageToStore(int id, int storeID, string message)
+        {
+            return _realBridge.WriteMessageToStore(id, storeID, message);
+        }
+
+        public Response ComplainToAdmin(int id, string message)
+        {
+            return _realBridge.ComplainToAdmin(id, message);
+        }
+
+        public Response GetPurchasesInfo(int id)
+        {
+            return _realBridge.GetPurchasesInfo(id);
+        }
+
+        public Response AddItemToStore(int id, int storeID, string itemName, string itemCategory, float itemPrice)
+        {
+            return _realBridge.AddItemToStore(id, storeID, itemName, itemCategory, itemPrice);
+        }
+
+        public Response RemoveItemFromStore(int id, int storeID, int itemID)
+        {
+            return _realBridge.RemoveItemFromStore(id, storeID, itemID);
+        }
+
+        public Response AppointStoreOwner(int id, int storeID, int newUserID)
+        {
+            return _realBridge.AppointStoreOwner(id, storeID, newUserID);
+        }
+
+        public Response AppointStoreManager(int id, int storeID, int newUserID)
+        {
+            return _realBridge.AppointStoreManager(id, storeID, newUserID);
+        }
+
+        public Response RemoveStoreOwner(int id, int storeID, int userID)
+        {
+            return _realBridge.RemoveStoreOwner(id, storeID, userID);
+        }
+
+        public Response RemovetStoreManager(int id, int storeID, int userID)
+        {
+            return _realBridge.RemovetStoreManager(id, storeID, userID);
+        }
+
+        public Response CloseStore(int id, int storeID)
+        {
+            return _realBridge.CloseStore(id, storeID);
+        }
+
+        public Response ReopenStore(int id, int storeID)
+        {
+            return _realBridge.ReopenStore(id, storeID);
+        }
+
+        public ResponseT<List<S_Member>> GetEmployeeInfoInStore(int id, int storeID)
+        {
+            return _realBridge.GetEmployeeInfoInStore(id, storeID);
+        }
+
+        public Response GetPurchasesInfo(int id, int storeID)
+        {
+            return _realBridge.GetPurchasesInfo(id, storeID);
+        }
+
+        public Response DeleteStore(int id, int storeID)
+        {
+            return _realBridge.DeleteStore(id, storeID);
+        }
+
+        public Response DeleteMember(int id, int userID)
+        {
+            return _realBridge.DeleteMember(id, userID);
         }
     }
 }
