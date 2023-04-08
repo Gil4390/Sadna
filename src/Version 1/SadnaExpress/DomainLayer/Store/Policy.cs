c4ccc49c31593b226c990fd906e0162a078d6363
unknown <radwan@post.bgu.ac.il>
2023-04-08 14:39:09 +0300

    Policy, PurchasePolicy, DiscountPolicy, Pair added

--- src/Version 1/SadnaExpress/DomainLayer/Store/Policy.cs
+++ src/Version 1/SadnaExpress/DomainLayer/Store/Policy.cs
@@ -2,6 +2,19 @@ namespace SadnaExpress.DomainLayer.Store
 {
     public class Policy
     {
-        
+        public virtual bool IsPurchasePolicyTakesPlace(int n)
+        {
+            return false;
+        }
+
+        public virtual bool HasDiscount()
+        {
+            return false;
+        }
+
+        public virtual Pair<int, int> GetDiscount()
+        {
+            return null;
+        }
     }
 }
--- src/Version 1/SadnaExpress/DomainLayer/Store/PurchasePolicy.cs
+++ src/Version 1/SadnaExpress/DomainLayer/Store/PurchasePolicy.cs
@@ -1,7 +1,44 @@
 namespace SadnaExpress.DomainLayer.Store
 {
-    public class PurchasePolicy
+    public class PurchasePolicy : Policy
     {
-        
+        public class MinPricePurchasePolicy : PurchasePolicy
+        {
+            private int minPrice;
+
+            public MinPricePurchasePolicy(int min)
+            {
+                this.minPrice = min;
+            }
+
+            public override bool IsPurchasePolicyTakesPlace(int price)
+            {
+                return price >= this.minPrice;
+            }
+        }
+
+        public class MinStockPurchasePolicy : PurchasePolicy
+        {
+            private int minStock;
+
+            public MinStockPurchasePolicy(int min)
+            {
+                this.minStock = min;
+            }
+
+            public override bool IsPurchasePolicyTakesPlace(int stock)
+            {
+                return stock >= this.minStock;
+            }
+        }
+
+        public class NoPurchasePolicy : PurchasePolicy
+        {
+            public override bool IsPurchasePolicyTakesPlace(int n)
+            {
+                return true;
+            }
+        }
+
     }
 }
