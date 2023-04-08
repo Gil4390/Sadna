32eaa807afec36fd695555203614af722bda63ae
unknown <radwan@post.bgu.ac.il>
2023-04-08 14:56:50 +0300

    ItemReview, PurchaseHistory added

---
+++ src/Version 1/SadnaExpress/DomainLayer/Store/ItemReview.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadnaExpress.DomainLayer.Store
+{
+    public class ItemReview
+    {
+        private string username;
+        private int storeId;
+        private int rating;
+        private string description;
+
+        public ItemReview(string username, int storeId, int rating, string description)
+        {
+            this.username = username;
+            this.storeId = storeId;
+            this.rating = rating;
+            this.description = description;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public int StoreId { get => storeId; set => storeId = value; }
+        public int Rating { get => rating; set => rating = value; }
+        public string Description { get => description; set => description = value; }
+
+    }
+}
---
+++ src/Version 1/SadnaExpress/DomainLayer/Store/PurchaseHistory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadnaExpress.DomainLayer.Store
+{
+    public class PurchaseHistory
+    {
+        private int purchaseId;
+        private string username;
+        private int storeId;
+        //                itemId,    amount , price
+        private List<Pair<int, Pair<int, int>>> items;
+        private DateTime date;
+
+        public PurchaseHistory(int purchaseId, string username, int storeId, DateTime date)
+        {
+            this.PurchaseId = purchaseId;
+            this.Username = username;
+            this.storeId = storeId;
+            this.Date = date;
+            Items = new List<Pair<int, Pair<int, int>>>();
+        }
+
+        public int PurchaseId { get => purchaseId; set => purchaseId = value; }
+        public string Username { get => username; set => username = value; }
+        public int StoreId { get => storeId; set => storeId = value; }
+        public List<Pair<int, Pair<int, int>>> Items { get => items; set => items = value; }
+        public DateTime Date { get => date; set => date = value; }
+
+    }
+}
