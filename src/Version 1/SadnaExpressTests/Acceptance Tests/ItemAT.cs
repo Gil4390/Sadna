eefef9b4034ef47889ab0f8b02c6293925927422
Gil Khais <gil4390@gmail.com>
2023-04-14 22:57:47 +0300

    changed itemAT

--- src/Version 1/SadnaExpressTests/Acceptance Tests/ItemAT.cs
+++ src/Version 1/SadnaExpressTests/Acceptance Tests/ItemAT.cs
@@ -17,22 +17,16 @@ namespace SadnaExpressTests.Integration_Tests
             _server = new Server();
             _server.activateAdmin();
         }
-        public void Login(int idx , string email , string pass)
-        {
-            Thread client1 = new Thread(() =>
-            {
-                _server.service.Enter();
-                _server.service.Register(idx, email, " tal", " galmor", pass);
-                _server.service.Login(idx, email, pass);
-            });
-            client1.Start();
-            client1.Join();
-        }
+
         public Guid OpenNewStore(int idx , string email , string pass , string store_name)
         {
             _server.activateAdmin();
-            Login(idx, email, pass);
-            _server.service.OpenNewStore(idx, store_name);
+
+            Guid guestID1 = _server.service.Enter().Value;
+            _server.service.Register(guestID1, email, " tal", " galmor", pass);
+            Guid memberID1 = _server.service.Login(guestID1, email, pass).Value;
+
+            _server.service.OpenNewStore(memberID1, store_name);
             foreach (Store store in _server.service.GetStores().Values)
             {
                 if (store.getName() == store_name)
