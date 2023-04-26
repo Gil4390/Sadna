4eefd7a52a0a4d6a7890ddc78148eab40a08149e
Gil Khais <gil4390@gmail.com>
2023-04-26 16:21:34 +0300

    added fields for filtering item serach on the shopping page

--- src/sadna-front/src/components/StoreItem.jsx
+++ src/sadna-front/src/components/StoreItem.jsx
@@ -3,15 +3,15 @@ import { Button, Card } from "react-bootstrap"
 
 
 export function StoreItem({ id, name, price }) {
+  const amountInCart = 0
   return (
     <Card className="h-100">
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="d-flex justify-content-between align-items-baseline mb-5">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
-        </Card.Title>
-        <Card.Text>
           <span className="ms-2 text-muted">{price} ₪</span>
-        </Card.Text>
+
+        </Card.Title>
         <Card.Text>
           <span className="ms-2 text-muted">items left: 5</span>
         </Card.Text>
--- src/sadna-front/src/pages/ShoppingPage.jsx
+++ src/sadna-front/src/pages/ShoppingPage.jsx
@@ -5,6 +5,10 @@ import { StoreItem } from '../components/StoreItem';
 function ShoppingPage() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchType, setSearchType] = useState('product');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [minStoreRating, setMinStoreRating] = useState('');
+  const [minItemRating, setMinItemRating] = useState('');
 
   const handleSearchQueryChange = (event) => {
     setSearchQuery(event.target.value);
@@ -14,10 +18,26 @@ function ShoppingPage() {
     setSearchType(event.target.value);
   };
 
+  const handleMinPriceChange = (event) => {
+    setMinPrice(event.target.value);
+  };
+
+  const handleMaxPriceChange = (event) => {
+    setMaxPrice(event.target.value);
+  };
+
+  const handleMinStoreRatingChange = (event) => {
+    setMinStoreRating(event.target.value);
+  };
+
+  const handleMinItemRatingChange = (event) => {
+    setMinItemRating(event.target.value);
+  };
+
   // handle search form submission
   const handleSearchSubmit = (event) => {
     event.preventDefault();
-    console.log(`Search Query: ${searchQuery}, Search Type: ${searchType}`);
+    console.log(`Search Query: ${searchQuery}, Search Type: ${searchType}`,`Min Price: ${minPrice}, Max Price: ${maxPrice}, Min Store Rating: ${minStoreRating}, Min Item Rating: ${minItemRating}`);
     // do something with search query and type
   };
 
@@ -34,38 +54,70 @@ function ShoppingPage() {
   ];
 
   return (
-    <Container>
-      <Row className="mt-3">
-        <Col>
-          <Form inline onSubmit={handleSearchSubmit}>
-            <Row>
-              <Col sm={6}>
-                <Form.Control type="text" placeholder="Search" className="mr-sm-2" value={searchQuery} onChange={handleSearchQueryChange} />
-              </Col>
-              <Col sm={2}>
-                <Form.Control as="select" className="mr-sm-2" value={searchType} onChange={handleSearchTypeChange}>
-                  <option value="product">Product</option>
-                  <option value="category">Category</option>
-                  <option value="brand">Brand</option>
-                </Form.Control>
-              </Col>
-              <Col>
-                <Button variant="outline-primary" type="submit">
-                  Search
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
-      <Row className="mt-3">
-        {items.map((item) => (
-          <Col sm={8} md={5} lg={4} xl={3} key={item.id} className="mt-3">
-            <StoreItem {...item} />
+    <div>
+      <Container>
+        <Row className="mt-3">
+          <Col>
+            <Form inline onSubmit={handleSearchSubmit}>
+              <Row>
+                <Col sm={6}>
+                  <Form.Control type="text" placeholder="Search" className="mr-sm-2" value={searchQuery} onChange={handleSearchQueryChange} />
+                </Col>
+                <Col sm={3}>
+                  <Form.Control as="select" className="mr-sm-2" value={searchType} onChange={handleSearchTypeChange}>
+                    <option value="product">Product</option>
+                    <option value="category">Category</option>
+                  </Form.Control>
+                </Col>
+                <Col>
+                  <Button variant="outline-primary" type="submit">
+                    Search
+                  </Button>
+                </Col>
+              </Row>
+              <Row style={{padding: "0.5rem"}}>
+                <Col sm={2}>
+                  <span className="fs-2">Min Price:</span>
+                  <Form.Control type="number" placeholder="0" value={minPrice} onChange={handleMinPriceChange} />
+                </Col>
+                <Col sm={2}>
+                  <span className="fs-2">Max Price:</span>
+                  <Form.Control type="number" value={maxPrice} onChange={handleMaxPriceChange} />
+                </Col>
+                <Col sm={2}>
+                  <span className="fs-2">Min Store Rating:</span>
+                  <Form.Control as="select" value={minStoreRating} onChange={handleMinStoreRatingChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Form.Control>
+                </Col>
+                <Col sm={2}>
+                  <span className="fs-2">Min Item Rating:</span>
+                  <Form.Control as="select" value={minItemRating} onChange={handleMinItemRatingChange}>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </Form.Control>
+                </Col>
+              </Row>
+            </Form>
           </Col>
-          ))}
-      </Row>
-    </Container>
+        </Row>
+        <Row className="mt-3">
+          {items.map((item) => (
+            <Col sm={8} md={5} lg={4} xl={3} key={item.id} className="mt-3">
+              <StoreItem {...item} />
+            </Col>
+            ))}
+        </Row>
+      </Container>
+      
+    </div>
   );
 }
 
