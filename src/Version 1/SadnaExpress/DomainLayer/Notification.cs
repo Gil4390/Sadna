1b524d8c05972a94c2aaf9e0b215f4228a067932
dinaagapov <agapov@post.bgu.ac.il>
2023-04-27 20:53:59 +0300

    adding new class - Notification.cs

---
+++ src/Version 1/SadnaExpress/DomainLayer/Notification.cs
@@ -0,0 +1,43 @@
+﻿namespace SadnaExpress.DomainLayer;
+using System;
+using NodaTime;
+
+    public class Notification
+    {
+        private DateTime time;
+        private Guid sentFrom;
+        private string message;
+        private Guid sentTo;
+
+        public Notification(DateTime time, Guid sentFrom, string message, Guid sentTo)
+        {
+            this.time = time;
+            this.sentFrom = sentFrom;
+            this.message = message;
+            this.sentTo = sentTo;
+        }
+
+        public Guid SentTo
+        {
+            get => sentTo;
+            set => sentTo = value;
+        }
+
+        public string Message
+        {
+            get => message;
+            set => message = value;
+        }
+
+        public Guid SentFrom
+        {
+            get => sentFrom;
+            set => sentFrom = value;
+        }
+
+        public DateTime Time
+        {
+            get => time;
+            set => time = value;
+        }
+    }
