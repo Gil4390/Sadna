e5000de4f4f1463a8e19b899163085355bb92baf
dinaagapov <agapov@post.bgu.ac.il>
2023-04-27 20:55:59 +0300

    for now the only kind of users who need to be updated about changes are storeOwners and this why I have only list of storeOwners(observers). this class is responsible for the notification.

---
+++ src/Version 1/SadnaExpress/DomainLayer/User/NotificationSystem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using NodaTime;
+using SadnaExpress.DomainLayer.User;
+
+namespace SadnaExpress.DomainLayer
+{
+    public class NotificationSystem : ISubject
+    {
+        private List<Member> storeOwners;
+        public NotificationSystem()
+        {
+            storeOwners = new List<Member>();
+        }
+
+
+
+        public void NotifyObservers(string message, Guid userId)
+        {
+            foreach (Member member in storeOwners)
+            {
+                Notification notification = new Notification(DateTime.Now, userId, message, member.UserId);
+                if (member.LoggedIn)
+                    member.showMessage();
+                else
+                    member.addToAwaitingNotification(notification);
+            }
+        }
+        
+        public void update(Member member, string message, Guid userId)
+        { 
+            Notification notification = new Notification(DateTime.Now, userId, message, member.UserId);
+            if (member.LoggedIn)
+                member.showMessage();
+            else
+                member.addToAwaitingNotification(notification);
+        }
+
+        public void RegisterObserver(Member observer)
+        {
+            storeOwners.Add(observer);
+        }
+        public void RegisterObservers(List<Member> observers)
+        {
+            foreach (Member member in observers)
+            {
+                storeOwners.Add(member);
+            }
+        }
+
+        public void RemoveObserver(Member observer)
+        {
+            storeOwners.Remove(observer);
+
+        }
+
+  
+    }
+}
