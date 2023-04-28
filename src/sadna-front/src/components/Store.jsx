3be601a472bb765b939d4350c23ec6bd66486bad
Gil Khais <gil4390@gmail.com>
2023-04-28 20:28:18 +0300

    store policies page start

--- src/sadna-front/src/components/Store.jsx
+++ src/sadna-front/src/components/Store.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 export function Store({ store }) {
   return (
     <div>
       <h2>{store.name}</h2>
-      <button>Edit Store</button>
+      <Button variant="dark" style={{margin: "5px"}}>
+        View Items
+      </Button>
+      <Button variant="dark" style={{margin: "5px"}}>
+        View Policies
+      </Button>
+      <Button variant="dark" style={{margin: "5px"}}>
+        View Employees
+      </Button>
     </div>
   );
 }
