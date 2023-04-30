410b0c875aba2133a58a500a12bfcd8b66a9dc3a
Gil Khais <gil4390@gmail.com>
2023-04-30 22:01:22 +0300

    manage items page added react components

--- src/sadna-front/src/pages/Member/ManageItemsPage.jsx
+++ src/sadna-front/src/pages/Member/ManageItemsPage.jsx
@@ -1,24 +1,170 @@
-import React from "react";
-import { Container, ListGroup, Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Button, Modal, Form } from 'react-bootstrap';
 
-function ManageItemsPage() {
+const items = [
+  { id: 1, name: 'Item 1', price: 10, category: 'Category 1', quantity: 5 },
+  { id: 2, name: 'Item 2', price: 20, category: 'Category 2', quantity: 10 },
+  { id: 3, name: 'Item 3', price: 30, category: 'Category 3', quantity: 15 },
+];
+
+const ManageItemsPage = () => {
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [editedItem, setEditedItem] = useState(null);
+  const [itemsList, setItemsList] = useState(items);
+
+  const handleAddModalClose = () => setShowAddModal(false);
+  const handleAddModalShow = () => setShowAddModal(true);
+
+  const handleEditModalClose = () => setShowEditModal(false);
+  const handleEditModalShow = (item) => {
+    setEditedItem(item);
+    setShowEditModal(true);
+  };
+
+  const handleAddItem = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newItem = {
+      id: Math.max(...itemsList.map((item) => item.id)) + 1,
+      name: formData.get('name'),
+      price: formData.get('price'),
+      category: formData.get('category'),
+      quantity: formData.get('quantity'),
+    };
+    setItemsList([...itemsList, newItem]);
+    setShowAddModal(false);
+  };
+
+  const handleEditItem = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const updatedItem = {
+      id: editedItem.id,
+      name: formData.get('name'),
+      price: formData.get('price'),
+      category: formData.get('category'),
+      quantity: formData.get('quantity'),
+    };
+    const updatedItemsList = itemsList.map((item) =>
+      item.id === editedItem.id ? updatedItem : item
+    );
+    setItemsList(updatedItemsList);
+    setShowEditModal(false);
+  };
+
   return (
-    <Container className="my-5">
-      <h1>Store Items:</h1>
-        <hr />
-        <ListGroup>
-          <h5>Create New Item</h5>
-        </ListGroup>
-        <div>
-          <ListGroup>
-            
-          </ListGroup>
-          
+    <div>
+      <h1>Manage Items</h1>
+      <Button variant="primary" onClick={handleAddModalShow} style={{margin: "0.5rem"}}>
+        Add New Item
+      </Button>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itemsList.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>{item.category}</td>
+              <td>{item.quantity}</td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => handleEditModalShow(item)}
+                >
+                  Edit
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
 
-          
-        </div>
-    </Container>
+      <Modal show={showAddModal} onHide={handleAddModalClose}>
+        <Modal.Header closeButton>
+          <h5 class="modal-title">Add New Item</h5>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleAddItem}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="number" placeholder="Enter price" />
+            </Form.Group>
+            <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control type="text" placeholder="Enter category" />
+            </Form.Group>
+            <Form.Group controlId="quantity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control type="number" placeholder="Enter quantity" />
+            </Form.Group>
+            <Button variant="primary" type="submit" style={{margin: "0.5rem"}}>
+              Add
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <Modal show={showEditModal} onHide={handleEditModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Item</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleEditItem}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                defaultValue={editedItem ? editedItem.name : ''}
+              />
+            </Form.Group>
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter price"
+                defaultValue={editedItem ? editedItem.price : ''}
+              />
+            </Form.Group>
+            <Form.Group controlId="category">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter category"
+                defaultValue={editedItem ? editedItem.category : ''}
+              />
+            </Form.Group>
+            <Form.Group controlId="quantity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter quantity"
+                defaultValue={editedItem ? editedItem.quantity : ''}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" style={{margin: "0.5rem"}}>
+              Save Changes
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
-}
+};
 
 export default ManageItemsPage;
+
