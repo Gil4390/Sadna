a4cc9ef021767251160d70e590f08b2b6b56c2d2
Gil Khais <gil4390@gmail.com>
2023-05-01 23:12:38 +0300

    add store option in store management page, but there is a bug where the name of the store wont appear on new stores

--- src/sadna-front/src/components/Store.jsx
+++ src/sadna-front/src/components/Store.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
-export function Store({ store }) {
+export function Store({ name }) {
   const navigate = useNavigate();
+  const [storeName, setStoreName] = useState(name);
   return (
     <div>
-      <h2>{store.name}</h2>
+      <h2>{storeName}</h2>
       <Button variant="dark" onClick={() => navigate("/ManageItemsPage")} style={{margin: "5px"}}>
         View Items
       </Button>
-      <Button variant="dark" onClick={() => navigate("/PoliciesPage")}style={{margin: "5px"}}>
+      <Button variant="dark" onClick={() => navigate("/PoliciesPage")} style={{margin: "5px"}}>
         View Policies
       </Button>
       <Button variant="dark" onClick={() => navigate("/ManageStoreEmployeesPage")} style={{margin: "5px"}} >
