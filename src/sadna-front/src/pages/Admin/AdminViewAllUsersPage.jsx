c4238abedb13aaec67ae9a8bd4e26af6731275ed
Gil Khais <gil4390@gmail.com>
2023-05-03 20:49:31 +0300

    added admin view all users page

--- src/sadna-front/src/pages/Admin/AdminViewAllUsersPage.jsx
+++ src/sadna-front/src/pages/Admin/AdminViewAllUsersPage.jsx
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
 
-function AdminViewAllUsersPage() {
+const AdminViewAllUsersPage = () => {
+  const [users, setUsers] = useState([
+    { id: 1, name: 'John Doe', email: 'john.doe@example.com', membershipType: 'member' },
+    { id: 2, name: 'Jane Doe', email: 'jane.doe@example.com', membershipType: 'guest' },
+    { id: 3, name: 'Bob Smith', email: 'bob.smith@example.com', membershipType: 'member' },
+    { id: 4, name: 'Alice Brown', email: 'alice.brown@example.com', membershipType: 'guest' },
+  ]);
+
+  useEffect(() => {
+    // const fetchUsers = async () => {
+    //   const response = await fetch('/api/users');
+    //   const data = await response.json();
+    //   setUsers(data);
+    // };
+
+    // fetchUsers();
+  }, []);
+
+  const handleRemoveUser = async (userId) => {
+    // Send a request to API to remove user
+    const response = await fetch(`/api/users/${userId}`, { method: 'DELETE' });
+    if (response.ok) {
+      // If user was removed successfully, update the state
+      setUsers((prevUsers) => prevUsers.filter((user) => user.id !== userId));
+    }
+  };
+
   return (
-    <div className="AdminViewAllUsersPage">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">AdminViewAllUsersPage</h1>
-            <p>
-              
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Membership Type</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.membershipType}</td>
+            <td>
+              {user.membershipType === 'member' && (
+                <Button variant="danger" onClick={() => handleRemoveUser(user.id)}>
+                  Remove
+                </Button>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
-}
+};
 
 export default AdminViewAllUsersPage;
